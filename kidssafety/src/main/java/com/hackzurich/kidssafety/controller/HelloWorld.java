0faadb2b71572f6112f3cc9c0f77031f06a8a4ac
Daniel Gehrig <daniel.gehrig1@gmx.ch>
2017-09-16 15:14:42 +0200

    renamed file and created dummy function in controller to display image

--- kidssafety/src/main/java/com/hackzurich/kidssafety/controller/HelloWorld.java
+++ kidssafety/src/main/java/com/hackzurich/kidssafety/controller/HelloWorld.java
@@ -1,21 +1,37 @@
 package com.hackzurich.kidssafety.controller;
 
+import com.hackzurich.kidssafety.service.HomeApp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.bind.annotation.XmlRegistry;
 
 @RestController
 @CrossOrigin
 public class HelloWorld {
 
+    @Autowired
+    HomeApp homeApp;
+
     @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public String toggle(@RequestParam boolean A) {
+        String a = homeApp.hello_world();
+        return a;
     }
 
     @RequestMapping("/home")
     public String home() {
         return "I am at home!";
+    }
+
+    @RequestMapping("/camera")
+    public BufferedImage get_image() {
+        homeApp.take_image();
+        BufferedImage image = homeApp.get_image();
+        return image;
     }
 
 
---
+++ kidssafety/src/main/java/com/hackzurich/kidssafety/service/HomeApp.java
@@ -0,0 +1,13 @@
+package com.hackzurich.kidssafety.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeApp {
+
+    public HomeApp() {}
+
+
+    public String hello_world() {return "Helsdfsdfsdlo World";}
+
+}
